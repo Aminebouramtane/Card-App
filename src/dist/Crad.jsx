992d7351b6b437f938ad051b2a15978734138e1a
amine <bouramtaneamine1@gmail.com>
2023-06-27 15:59:28 +0100

    fix style of cards

---
+++ src/dist/Crad.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./style.css"
+const Crad = () => {
+  return (
+    <>
+        <section>
+			<div className="row">
+				<div className="col-md-4 col-sm-6 col-xs-12">
+					<div className="card">
+						<div className="cover item-a">
+							<h1>Little<br/>Bonsai</h1>
+							<span className="price">$79</span>
+							<div className="card-back">
+								<a href="#">Add to cart</a>
+								<a href="#">View detail</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>		
+    </>
+  )
+}
+
+export default Crad
